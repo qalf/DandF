3aded7dec9975c96cd824449a5dc69d1c299a8f4
Lucas Fleury <lucas.fleury@cpe.fr>
2020-11-28 18:15:23 +0100

    dossier image et affichage tous les continents

--- javascript/script.js
+++ javascript/script.js
@@ -12,12 +12,12 @@ class continent {
     }
 }
 
-let Europe = new continent(0, 'images/Continent/Europe.jpeg', 'Europe', 'Europe');
-let Asie = new continent(1, 'images/Continent/Asie.jpeg', 'Asie', 'Asie');
-let AmériqueN = new continent(2, 'images/Continent/AmériqueN.jpg', 'AmériqueN', 'Amérique Du Nord');
-let AmériqueS = new continent(3, 'images/Continent/AmériqueS.jpeg', 'AmériqueS', 'Amérique du Sud');
-let Afrique = new continent(4, 'images/Continent/Afrique.jpeg', 'Afrique', 'Afrique');
-let IlesP = new continent(5, 'images/Continent/IlesP.jpeg', 'IlesP', 'Iles Paradisiaques');
+let Europe = new continent(0, 'images/Continent/Europe.jpeg', 'html/Europe.html', 'Europe');
+let Asie = new continent(1, 'images/Continent/Asie.jpeg', 'Asie', 'html/Asie.html');
+let AmériqueN = new continent(2, 'images/Continent/AmériqueN.jpg', 'html/AmériqueN.html', 'Amérique Du Nord');
+let AmériqueS = new continent(3, 'images/Continent/AmériqueS.jpeg', 'html/AmériqueS.html', 'Amérique du Sud');
+let Afrique = new continent(4, 'images/Continent/Afrique.jpeg', 'html/Afrique.html', 'Afrique');
+let IlesP = new continent(5, 'images/Continent/IlesP.jpeg', 'html/IlesP.html', 'Iles Paradisiaques');
 
 class ville{
     constructor(id,link,name,continent){
@@ -34,37 +34,89 @@ let Londres = new ville(2, '../images/Europe/Londres.jpg', 'Londres', 'Europe');
 let Rome = new ville(3, '../images/Europe/Rome.jpg', 'Rome', 'Europe');
 let Stockholm = new ville(4, '../images/Europe/Stockholm.jpg', 'Stockholm', 'Europe');
 
-let Bankok = new ville(0, '../images/Asie/Bankok.jpg', 'Bankok', 'Asie');
+let Bangkok = new ville(0, '../images/Asie/Bangkok.jpg', 'Bangkok', 'Asie');
 let Bombay = new ville(1, '../images/Asie/Bombay.jpg', 'Bombay', 'Asie');
 let Hanoï = new ville(2, '../images/Asie/Hanoï.jpg', 'Hanoï', 'Asie');
 let HongKong = new ville(3, '../images/Asie/HongKong.jpg', 'Hong-Kong', 'Asie');
 let Pékin = new ville(4, '../images/Asie/Pékin.jpg', 'Pékin', 'Asie');
 let Tokyo = new ville(5, '../images/Asie/Tokyo.jpg', 'Tokyo', 'Asie');
 
+let Cancun = new ville(0, '../images/AmériqueN/Cancun.jpg', 'Cancun', 'AmériqueN');
+let LosAngeles = new ville(1, '../images/AmériqueN/LosAngeles.jpg', 'Los Angeles', 'AmériqueN');
+let Miami = new ville(2, '../images/AmériqueN/Miami.jpg', 'Miami', 'AmériqueN');
+let Montreal = new ville(3, '../images/AmériqueN/Montreal.jpg', 'Montreal', 'AmériqueN');
+let NewYork = new ville(4, '../images/AmériqueN/NewYork.jpg', 'New York', 'AmériqueN');
+
+let BuenosAires = new ville(0, '../images/AmériqueS/BuenosAires.jpg', 'Buenos-Aires', 'AmériqueS');
+let LacTiticaca = new ville(1, '../images/AmériqueS/LacTiticaca.jpg', 'Lac Titicaca', 'AmériqueS');
+let Lima = new ville(2, '../images/AmériqueS/Lima.jpg', 'Lima', 'AmériqueS');
+let Rio = new ville(3, '../images/AmériqueS/Rio.jpg', 'Rio de Janeiro', 'AmériqueS');
+
+let CapeTown = new ville(0, '../images/Afrique/CapeTown.jpg', 'Cape Town', 'Afrique');
+let Casablanca = new ville(1, '../images/Afrique/Casablanca.jpg', 'Casablanca', 'Afrique');
+let Dubai = new ville(2, '../images/Afrique/Dubai.jpg', 'Dubaï', 'Afrique');
+let LeCaire = new ville(3, '../images/Afrique/LeCaire.jpg', 'Le Caire', 'Afrique');
+let Marrakesh = new ville(4, '../images/Afrique/Marrakesh.jpg', 'Marrakesh', 'Afrique');
+
+let Auckland = new ville(0, '../images/IlesP/Auckland.jpg', 'Auckland', 'IlesP');
+let Caraibes = new ville(1, '../images/IlesP/Caraibes.jpg', 'Caraïbes', 'IlesP');
+let Hawai = new ville(2, '../images/IlesP/Hawai.jpg', 'Hawaï', 'IlesP');
+let Seychelles = new ville(3, '../images/IlesP/Seychelles.jpg', 'Seychelles', 'IlesP');
+let Singapour = new ville(4, '../images/IlesP/Singapour.jpg', 'Singapour', 'IlesP');
+let Ubud = new ville(5, '../images/IlesP/Ubud.jpg', 'Ubud', 'IlesP');
+
 
 let LstContinent = [Europe, Asie,AmériqueN,AmériqueS,Afrique,IlesP];
-let LstEurope = [Paris,Barcelone,Londres,Rome,Stockholm];
-let LstAsie =[Bankok,Bombay,Hanoï,HongKong,Pékin,Tokyo]
 
+let LstEurope = [Paris,Barcelone,Londres,Rome,Stockholm];
+let LstAsie = [Bankok,Bombay,Hanoï,HongKong,Pékin,Tokyo];
+let LstAmériqueN = [Cancun, LosAngeles, Miami,Montreal,NewYork];
+let LstAmériqueS = [BuenosAires,LacTiticaca,Lima,Rio];
+let LstAfrique = [CapeTown,Casablanca,Dubai,LeCaire,Marrakesh];
+let LstIlesP = [Auckland,Caraibes,Hawai,Seychelles,Singapour,Ubud];
 
 afficheimg();
 function afficheimg() {
     let link = ''
-    if (URL === 'https://qalf.github.io/DandF/Europe.html'){
-        for (let i of LstEurope) {
+    if (URL === 'https://qalf.github.io/DandF/html/Europe.html'){
+        for (var i of LstEurope) {
             link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
         }
         affichage.innerHTML += link;
     }
 
-    if (URL === 'https://qalf.github.io/DandF/Asie.html'){
-        for (let i of LstAsie) {
+    else if (URL === 'https://qalf.github.io/DandF/html/Asie.html'){
+        for (var i of LstAsie) {
+            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+        }
+        affichage.innerHTML += link;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/AmériqueN.html'){
+        for (var i of LstAsie) {
+            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+        }
+        affichage.innerHTML += link;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/AmériqueS.html'){
+        for (var i of LstAsie) {
+            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+        }
+        affichage.innerHTML += link;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/Afrique.html'){
+        for (var i of LstAsie) {
+            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+        }
+        affichage.innerHTML += link;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/IlesP.html'){
+        for (var i of LstAsie) {
             link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
         }
         affichage.innerHTML += link;
     }
     else {
-        for (let i of LstContinent) {
+        for (var i of LstContinent) {
             link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
         }
         affichage.innerHTML += link;
@@ -101,13 +153,39 @@ function delimg(i) {
 }
 
 function resa(i) {
-    if (URL === 'https://qalf.github.io/DandF/Europe.html') {
-        var lien = 'Résa.html';
+    if (URL === 'https://qalf.github.io/DandF/html/Europe.html') {
+        var id = LstEurope[i].id
+        var lien = 'https://qalf.github.io/DandF/html/Résa.html?id=' + id;
+        document.location.href = lien;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/Asie.html') {
+        var id = LstAsie[i].id
+        var lien = 'https://qalf.github.io/DandF/html/Résa.html?id=' + id;
+        document.location.href = lien;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/AmériqueN.html') {
+        var id = LstAmériqueN[i].id
+        var lien = 'https://qalf.github.io/DandF/html/Résa.html?id=' + id;
+        document.location.href = lien;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/AmériqueS.html') {
+        var id = LstAmériqueS[i].id
+        var lien = 'https://qalf.github.io/DandF/html/Résa.html?id=' + id;
+        document.location.href = lien;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/Afrique.html') {
+        var id = LstAfrique[i].id
+        var lien = 'https://qalf.github.io/DandF/html/Résa.html?id=' + id;
+        document.location.href = lien;
+    }
+    else if (URL === 'https://qalf.github.io/DandF/html/IlesP.html') {
+        var id = LstIlesP[i].id
+        var lien = 'https://qalf.github.io/DandF/html/Résa.html?id=' + id;
         document.location.href = lien;
     }
     else {
         var chemin = LstContinent[i].NomChemin;
-        var lien = chemin+'.html';
+        var lien = 'https://qalf.github.io/DandF/'+chemin;
         document.location.href = lien;
     }
 
