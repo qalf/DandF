fe1ebf0b7db1648cf5ce7b67a6bbecfbd9e5ea84
Lucas Fleury <lucas.fleury@cpe.fr>
2020-11-29 16:27:59 +0100

    id_continent

--- javascript/script.js
+++ javascript/script.js
@@ -21,8 +21,9 @@ let Afrique = new continent(4, 'images/Continent/Afrique.jpeg', 'html/Afrique.ht
 let IlesP = new continent(5, 'images/Continent/IlesP.jpeg', 'html/IlesP.html', 'Iles Paradisiaques');
 
 class ville{
-    constructor(id,link,name,continent,prix){
+    constructor(id,id_continent,link,name,continent,prix){
         this.id = id;
+        this.id_continent = id_continent;
         this.link = link;
         this.name = name;
         this.continent = continent
@@ -30,42 +31,42 @@ class ville{
     }
 }
 
-let Paris = new ville(0, '../images/Europe/Paris.jpg', 'Paris', 'Europe', 400);
-let Barcelone = new ville(1, '../images/Europe/Barcelone.jpg', 'Barcelone', 'Europe', 370);
-let Londres = new ville(2, '../images/Europe/Londres.jpg', 'Londres', 'Europe', 380);
-let Rome = new ville(3, '../images/Europe/Rome.jpg', 'Rome', 'Europe', 360);
-let Stockholm = new ville(4, '../images/Europe/Stockholm.jpg', 'Stockholm', 'Europe', 200);
+let Paris = new ville(0,0, '../images/Europe/Paris.jpg', 'Paris', 'Europe', 400);
+let Barcelone = new ville(1,1, '../images/Europe/Barcelone.jpg', 'Barcelone', 'Europe', 370);
+let Londres = new ville(2,2, '../images/Europe/Londres.jpg', 'Londres', 'Europe', 380);
+let Rome = new ville(3,3, '../images/Europe/Rome.jpg', 'Rome', 'Europe', 360);
+let Stockholm = new ville(4,4, '../images/Europe/Stockholm.jpg', 'Stockholm', 'Europe', 200);
 
-let Bangkok = new ville(5, '../images/Asie/Bangkok.jpg', 'Bangkok', 'Asie', 500);
-let Bombay = new ville(6, '../images/Asie/Bombay.jpg', 'Bombay', 'Asie', 550);
-let Hanoï = new ville(7, '../images/Asie/Hanoï.jpg', 'Hanoï', 'Asie', 475);
-let HongKong = new ville(8, '../images/Asie/HongKong.jpg', 'Hong-Kong', 'Asie', 570);
-let Pékin = new ville(9, '../images/Asie/Pékin.jpg', 'Pékin', 'Asie', 530);
-let Tokyo = new ville(10, '../images/Asie/Tokyo.jpg', 'Tokyo', 'Asie', 600);
+let Bangkok = new ville(5,0, '../images/Asie/Bangkok.jpg', 'Bangkok', 'Asie', 500);
+let Bombay = new ville(6,1, '../images/Asie/Bombay.jpg', 'Bombay', 'Asie', 550);
+let Hanoï = new ville(7,2, '../images/Asie/Hanoï.jpg', 'Hanoï', 'Asie', 475);
+let HongKong = new ville(8,3, '../images/Asie/HongKong.jpg', 'Hong-Kong', 'Asie', 570);
+let Pékin = new ville(9,4, '../images/Asie/Pékin.jpg', 'Pékin', 'Asie', 530);
+let Tokyo = new ville(10,5, '../images/Asie/Tokyo.jpg', 'Tokyo', 'Asie', 600);
 
-let Cancun = new ville(11, '../images/AmériqueN/Cancun.jpg', 'Cancun', 'AmériqueN', 450);
-let LosAngeles = new ville(12, '../images/AmériqueN/LosAngeles.jpg', 'Los Angeles', 'AmériqueN', 700);
-let Miami = new ville(13, '../images/AmériqueN/Miami.jpg', 'Miami', 'AmériqueN', 750);
-let Montreal = new ville(14, '../images/AmériqueN/Montreal.jpg', 'Montreal', 'AmériqueN', 500);
-let NewYork = new ville(15, '../images/AmériqueN/NewYork.jpeg', 'New York', 'AmériqueN', 700);
+let Cancun = new ville(11,0, '../images/AmériqueN/Cancun.jpg', 'Cancun', 'AmériqueN', 450);
+let LosAngeles = new ville(12,1, '../images/AmériqueN/LosAngeles.jpg', 'Los Angeles', 'AmériqueN', 700);
+let Miami = new ville(13,2, '../images/AmériqueN/Miami.jpg', 'Miami', 'AmériqueN', 750);
+let Montreal = new ville(14,3, '../images/AmériqueN/Montreal.jpg', 'Montreal', 'AmériqueN', 500);
+let NewYork = new ville(15,4, '../images/AmériqueN/NewYork.jpeg', 'New York', 'AmériqueN', 700);
 
-let BuenosAires = new ville(16, '../images/AmériqueS/BuenosAires.jpg', 'Buenos-Aires', 'AmériqueS', 470);
-let LacTiticaca = new ville(17, '../images/AmériqueS/LacTiticaca.jpg', 'Lac Titicaca', 'AmériqueS', 300);
-let Lima = new ville(18, '../images/AmériqueS/Lima.jpg', 'Lima', 'AmériqueS', 350);
-let Rio = new ville(19, '../images/AmériqueS/Rio.jpeg', 'Rio de Janeiro', 'AmériqueS', 320);
+let BuenosAires = new ville(16,0, '../images/AmériqueS/BuenosAires.jpg', 'Buenos-Aires', 'AmériqueS', 470);
+let LacTiticaca = new ville(17,1, '../images/AmériqueS/LacTiticaca.jpg', 'Lac Titicaca', 'AmériqueS', 300);
+let Lima = new ville(18,2, '../images/AmériqueS/Lima.jpg', 'Lima', 'AmériqueS', 350);
+let Rio = new ville(19,3, '../images/AmériqueS/Rio.jpeg', 'Rio de Janeiro', 'AmériqueS', 320);
 
-let CapeTown = new ville(20, '../images/Afrique/CapeTown.jpg', 'Cape Town', 'Afrique', 360);
-let Casablanca = new ville(21, '../images/Afrique/Casablanca.jpeg', 'Casablanca', 'Afrique', 450);
-let Dubai = new ville(22, '../images/Afrique/Dubai.jpg', 'Dubaï', 'Afrique', 900);
-let LeCaire = new ville(23, '../images/Afrique/LeCaire.jpg', 'Le Caire', 'Afrique', 480);
-let Marrakesh = new ville(24, '../images/Afrique/Marrakesh.jpg', 'Marrakesh', 'Afrique', 430);
+let CapeTown = new ville(20,0, '../images/Afrique/CapeTown.jpg', 'Cape Town', 'Afrique', 360);
+let Casablanca = new ville(21,1, '../images/Afrique/Casablanca.jpeg', 'Casablanca', 'Afrique', 450);
+let Dubai = new ville(22,2, '../images/Afrique/Dubai.jpg', 'Dubaï', 'Afrique', 900);
+let LeCaire = new ville(23,3, '../images/Afrique/LeCaire.jpg', 'Le Caire', 'Afrique', 480);
+let Marrakesh = new ville(24,4, '../images/Afrique/Marrakesh.jpg', 'Marrakesh', 'Afrique', 430);
 
-let Auckland = new ville(25, '../images/IlesP/Auckland.jpg', 'Auckland', 'IlesP', 820);
-let Caraibes = new ville(26, '../images/IlesP/Caraibes.jpg', 'Caraïbes', 'IlesP', 920);
-let Hawai = new ville(27, '../images/IlesP/Hawai.jpg', 'Hawaï', 'IlesP', 850);
-let Seychelles = new ville(28, '../images/IlesP/Seychelles.jpg', 'Seychelles', 'IlesP', 870);
-let Singapour = new ville(29, '../images/IlesP/Singapour.jpg', 'Singapour', 'IlesP', 950);
-let Ubud = new ville(30, '../images/IlesP/Ubud.jpg', 'Ubud', 'IlesP', 880);
+let Auckland = new ville(25,0, '../images/IlesP/Auckland.jpg', 'Auckland', 'IlesP', 820);
+let Caraibes = new ville(26,1, '../images/IlesP/Caraibes.jpg', 'Caraïbes', 'IlesP', 920);
+let Hawai = new ville(27,2, '../images/IlesP/Hawai.jpg', 'Hawaï', 'IlesP', 850);
+let Seychelles = new ville(28,3, '../images/IlesP/Seychelles.jpg', 'Seychelles', 'IlesP', 870);
+let Singapour = new ville(29,4, '../images/IlesP/Singapour.jpg', 'Singapour', 'IlesP', 950);
+let Ubud = new ville(30,5, '../images/IlesP/Ubud.jpg', 'Ubud', 'IlesP', 880);
 
 
 let LstContinent = [Europe,Asie,AmériqueN,AmériqueS,Afrique,IlesP];
@@ -120,42 +121,42 @@ if (URL === 'https://qalf.github.io/DandF/html/Asie.html'){
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[5].innerHTML = Bangkok.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[0].innerHTML = Bangkok.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Bombay&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[6].innerHTML = Bombay.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[1].innerHTML = Bombay.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Hanoï&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[7].innerHTML = Hanoï.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[2].innerHTML = Hanoï.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=HongKong&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[8].innerHTML = HongKong.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[3].innerHTML = HongKong.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Pékin&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[9].innerHTML = Pékin.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[4].innerHTML = Pékin.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[10].innerHTML = Tokyo.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[5].innerHTML = Tokyo.name +'<br>'+ Math.round(temp) +'°C';
     })
 }
 
@@ -164,35 +165,35 @@ if (URL === 'https://qalf.github.io/DandF/html/AmeriqueN.html'){
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[11].innerHTML = Cancun.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[0].innerHTML = Cancun.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Los Angeles&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[12].innerHTML = LosAngeles.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[1].innerHTML = LosAngeles.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Miami&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[13].innerHTML = Miami.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[2].innerHTML = Miami.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Montreal&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[14].innerHTML = Montreal.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[3].innerHTML = Montreal.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=New York&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[15].innerHTML = NewYork.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[4].innerHTML = NewYork.name +'<br>'+ Math.round(temp) +'°C';
     })
 }
 
@@ -201,28 +202,28 @@ if (URL === 'https://qalf.github.io/DandF/html/AmeriqueS.html'){
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[16].innerHTML = BuenosAires.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[0].innerHTML = BuenosAires.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Puno&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[17].innerHTML = LacTiticaca.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[1].innerHTML = LacTiticaca.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Lima&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[18].innerHTML = Lima.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[2].innerHTML = Lima.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Rio de Janeiro&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[19].innerHTML = Rio.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[3].innerHTML = Rio.name +'<br>'+ Math.round(temp) +'°C';
     })
 
 }
@@ -232,35 +233,35 @@ if (URL === 'https://qalf.github.io/DandF/html/Afrique.html'){
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[20].innerHTML = CapeTown.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[0].innerHTML = CapeTown.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Casablanca&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[21].innerHTML = Casablanca.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[1].innerHTML = Casablanca.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Dubai&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[22].innerHTML = Dubai.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[2].innerHTML = Dubai.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Le Caire&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[23].innerHTML = LeCaire.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[3].innerHTML = LeCaire.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Marrakesh&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[24].innerHTML = Marrakesh.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[4].innerHTML = Marrakesh.name +'<br>'+ Math.round(temp) +'°C';
     })
 }
 
@@ -269,42 +270,42 @@ if (URL === 'https://qalf.github.io/DandF/html/IlesP.html'){
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[25].innerHTML = Auckland.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[0].innerHTML = Auckland.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Bridgetown&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[26].innerHTML = Caraibes.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[1].innerHTML = Caraibes.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Hawai&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[17].innerHTML = Hawai.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[2].innerHTML = Hawai.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Seychelles&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[28].innerHTML = Seychelles.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[3].innerHTML = Seychelles.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Singapour&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[29].innerHTML = Singapour.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[4].innerHTML = Singapour.name +'<br>'+ Math.round(temp) +'°C';
     })
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Ubud&units=metric&appid=aaed9a489f3afb122bc1ac8d09c79637')
         .then(res => res.json())
         .then(data => {
             var temp = data.main.temp;
-            overlay[30].innerHTML = Ubud.name +'<br>'+ Math.round(temp) +'°C';
+            overlay[5].innerHTML = Ubud.name +'<br>'+ Math.round(temp) +'°C';
     })
 }
 
@@ -314,38 +315,38 @@ function afficheimg() {
     let link = ''
     if (URL === 'https://qalf.github.io/DandF/html/Europe.html'){
         for (var i of LstEurope) {
-            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay"></div></div>';
+            link += '<div class ="image" onmouseover="infosimg('+ i.id_continent +')" style=background-image:url("'+ i.link +'") ><div class = "overlay"></div></div>';
         }
         affichage.innerHTML += link;
     }
 
     else if (URL === 'https://qalf.github.io/DandF/html/Asie.html'){
         for (var i of LstAsie) {
-            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+            link += '<div class ="image" onmouseover="infosimg('+ i.id_continent +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
         }
         affichage.innerHTML += link;
     }
     else if (URL === 'https://qalf.github.io/DandF/html/AmeriqueN.html'){
         for (var i of LstAmériqueN) {
-            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+            link += '<div class ="image" onmouseover="infosimg('+ i.id_continent +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
         }
         affichage.innerHTML += link;
     }
     else if (URL === 'https://qalf.github.io/DandF/html/AmeriqueS.html'){
         for (var i of LstAmériqueS) {
-            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+            link += '<div class ="image" onmouseover="infosimg('+ i.id_continent +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
         }
         affichage.innerHTML += link;
     }
     else if (URL === 'https://qalf.github.io/DandF/html/Afrique.html'){
         for (var i of LstAfrique) {
-            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+            link += '<div class ="image" onmouseover="infosimg('+ i.id_continent +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
         }
         affichage.innerHTML += link;
     }
     else if (URL === 'https://qalf.github.io/DandF/html/IlesP.html'){
         for (var i of LstIlesP) {
-            link += '<div class ="image" onmouseover="infosimg('+ i.id +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
+            link += '<div class ="image" onmouseover="infosimg('+ i.id_continent +')" style=background-image:url("'+ i.link +'") ><div class = "overlay">'+ i.name +'</div></div>';
         }
         affichage.innerHTML += link;
     }
