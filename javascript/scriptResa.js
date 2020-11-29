cd1dfe1b428fc3ef79c5492891cad08de7276869
Lucas Fleury <lucas.fleury@cpe.fr>
2020-11-29 15:43:54 +0100

    id

--- javascript/scriptResa.js
+++ javascript/scriptResa.js
@@ -1,6 +1,6 @@
 let sejour_id = new URLSearchParams(window.location.search).get("id");
 
-for (let i = 0;1;30){
+for (var i = 0;1;30){
   var Ville = document.getElementsByClassName("ville").value;
   console.log(Ville);
   if (i === sejour_id){
