a70f9f0351bdcef0517ff089ea0512d07f2488ba
Lucas Fleury <lucas.fleury@cpe.fr>
2020-12-03 23:42:19 +0100

    console log

--- javascript/script.js
+++ javascript/script.js
@@ -324,6 +324,9 @@ if (path == '/DandF/html/Espace-perso.html'){
         	.then(res => res.json())
         	.then(data => {
                 console.log(data);
+                console.log(data['members'][0]['name']);
+                console.log(data['members'][1]['mdp']);
+
                 for (var i = 0;i<data.length+1;i++){
                     if (data['members'][i]['name'] == utilisateur.value && data['members'][i]['mdp'] == mdp.value){
                         connect();
